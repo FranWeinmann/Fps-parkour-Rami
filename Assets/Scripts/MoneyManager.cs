97ec14e18377adb1464da90dc41a319e452bd12b
FranWeinmann <weinmann.fran@gmail.com>
2025-05-30 10:38:33 -0300

    Avances¿

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+    {
+        if (money + amount < 0)
+        {
+            // Impedir la compra
+            Debug.Log("Dinero insuficiente");
+        }
+        else
+        {
+            money += amount;
+        }
+    }
+}
